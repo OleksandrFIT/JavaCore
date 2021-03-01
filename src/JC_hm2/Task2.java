de4052d93c1f041a3f8a338a00e3126300f760db
OleksandrFIT <dragan.sasha2002@gmail.com>
2021-03-01 11:00:06 -0500

    Java Core homework

--- src/JC_hm2/Task2.java
+++ src/JC_hm2/Task2.java
@@ -1,7 +1,35 @@
 package JC_hm2;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList();
+        System.out.println("Заповніть масив ");
+        Scanner scArray= new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("Введіть " + i + " елемент:");
+            int sc= scArray.nextInt();
+            nums.add(sc);
+        }
+
+        System.out.println(nums);
+        Collections.reverse(nums);
+        System.out.println(nums);
+
+
+
+
+        /*
+        System.out.print ("Масив:");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print (" " + nums[i]);
+        }*/
 
 
 
